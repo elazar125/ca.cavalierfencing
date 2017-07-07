5a8c67ebeeb11f6e89d6a05365201d1f222f2cc0
elazar125 <elazar125@gmail.com>
2017-07-06 21:56:26 -0500

    set up files

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('sass', function () {
+  return gulp.src('./app/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./app/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('js', function () {
+  gulp.src(['./app/**/module.js', './app/**/*.js'])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./app'))
+});
+
+gulp.task('js:watch', function () {
+  gulp.watch('./app/**/*.js', ['js']);
+});
+
+gulp.task('build', [ 'js', 'sass' ]);
+gulp.task('watch', [ 'js:watch', 'sass:watch' ]);
